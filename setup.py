43ddd78b626e2ef06c54bd54d281ca5285f62731
Rafael Laverde <leafar91@gmail.com>
2017-04-19 17:04:04 -0500

    Add pweave basic support.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def get_description():
     return data
 
 
-REQUIREMENTS = ['spyder']
+REQUIREMENTS = ['spyder', 'pweave']
 
 
 setup(
--- spyder_reports/widgets/reportsgui.py
+++ spyder_reports/widgets/reportsgui.py
@@ -9,9 +9,30 @@
 
 from qtpy.QtWidgets import QWidget
 
+from spyder.widgets.browser import WebView
+
+
+class RenderView(WebView):
+
+    def __init__(self, parent):
+        WebView.__init__(self, parent)
+
+
 class ReportsWidget(QWidget):
     """Reports widget."""
     def __init__(self, parent):
         QWidget.__init__(self, parent)
 
         self.setWindowTitle("Reports")
+
+        self.renderview = RenderView(self)
+
+    def set_html(self, html_text, base_url=None):
+        """Set html text"""
+        if base_url is not None:
+            self.renderview.setHtml(html_text, base_url)
+        else:
+            self.renderview.setHtml(html_text)
+
+    def clear(self):
+        self.set_html('')
