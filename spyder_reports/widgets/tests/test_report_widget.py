e06c7285ccfab7795099c3b82b354e61f963e3e6
Rafael Laverde <leafar91@gmail.com>
2017-07-12 13:41:59 -0500

    Add test for closing tabs.

--- spyder_reports/widgets/tests/test_report_widget.py
+++ spyder_reports/widgets/tests/test_report_widget.py
@@ -7,6 +7,7 @@
 
 # Third party imports
 import pytest
+from qtpy.QtCore import Qt
 
 # Spyder-IDE and Local imports
 from spyder_reports.widgets.reportsgui import ReportsWidget
@@ -55,6 +56,35 @@ def test_open_several_tabs(qtbot):
 
     # It shouldn't open a new tab because the file was already rendered
     assert reports.tabs.count() == 2
+
+
+def test_close_tabs(qtbot):
+    """Test closing tabs.
+    When a tab is closed also the reference to the renderview should be removed.
+    """
+    reports = setup_reports(qtbot)
+
+    def close_tab(index):
+        """Find the close button and click it"""
+        for i in [0, 1]:  # LeftSide, RightSide
+            close_button = reports.tabs.tabBar().tabButton(index, i)
+            if close_button:
+                break
+        qtbot.mouseClick(close_button, Qt.LeftButton)
+
+    reports.set_html('some html', 'file1')
+    reports.set_html('some html', 'file2')
+    assert reports.tabs.count() == 2
+
+    # close 'file2'
+    close_tab(1)
+    assert reports.tabs.count() == 1
+    assert reports.renderviews.get('file2') is None
+
+    # close 'file1'
+    close_tab(0)
+    assert reports.tabs.count() == 0
+    assert reports.renderviews.get('file1') is None
 
 
 if __name__ == "__main__":
