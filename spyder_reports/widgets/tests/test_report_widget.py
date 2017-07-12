40a3143785a757c93e94f00f4e73b48839fe3f8d
Rafael Laverde <leafar91@gmail.com>
2017-07-12 12:57:09 -0500

    Add test for overwriting at start.

--- spyder_reports/widgets/tests/test_report_widget.py
+++ spyder_reports/widgets/tests/test_report_widget.py
@@ -26,5 +26,19 @@ def test_reports(qtbot):
     assert reports
 
 
+def test_overwrite_welcome(qtbot):
+    """Test taht the first rendering doesn't create a new tab.
+    It should overwrite 'Welcome' tab instead.
+    """
+    reports = setup_reports(qtbot)
+
+    renderview = reports.renderviews['Welcome']
+    reports.set_html('some html', 'file name')
+
+    assert reports.tabs.count() == 1
+    assert len(reports.renderviews) == 1
+    assert reports.renderviews['file name'] == renderview
+
+
 if __name__ == "__main__":
     pytest.main()
